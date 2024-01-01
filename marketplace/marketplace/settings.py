c47e63b97769315a816803773a68cfeb44d00cba
Vladimir Ermakov <jelasswot@gmail.com>
2024-01-01 21:19:28 +0300

    add media

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -139,6 +139,10 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
