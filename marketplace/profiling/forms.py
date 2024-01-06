3ee41e1d28bd557213a8559339b268ce76a19019
Vladimir Ermakov <jelasswot@gmail.com>
2024-01-06 23:47:21 +0300

    improvement login form

--- marketplace/profiling/forms.py
+++ marketplace/profiling/forms.py
@@ -5,24 +5,32 @@ from .models import User
 
 
 class UserLoginForm(AuthenticationForm):
+
     username = UsernameField(
         label="",
-        widget=forms.TextInput(attrs={
-            "autofocus": True,
-            "class": "form-control",
-            "placeholder": "username",
-        })
     )
     password = forms.CharField(
         label="",
-        widget=forms.PasswordInput(attrs={
-            "autocomplete": "current-password",
-            "class": "form-control",
-            "placeholder": "password",
-        }),
     )
+
+    # username = UsernameField(
+    #     label="",
+    #     widget=forms.TextInput(attrs={
+    #         "autofocus": True,
+    #         "class": "form-control",
+    #         "placeholder": "username",
+    #     })
+    # )
+    # password = forms.CharField(
+    #     label="",
+    #     widget=forms.PasswordInput(attrs={
+    #         "autocomplete": "current-password",
+    #         "class": "form-control",
+    #         "placeholder": "password",
+    #     }),
+    # )
     error_messages = {
-        'invalid_login': ("Please enter a correct %(username)s and password."),
+        'invalid_login': ("Please enter a correct username and password."),
         'inactive': ("This account is inactive."),
     }
 
