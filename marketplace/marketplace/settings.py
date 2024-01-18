fe1fe11e437b439da18f5f9c30dfa3075142c118
Vladimir Ermakov <145784290+waldemarX@users.noreply.github.com>
2024-01-18 15:29:35 +0300

    add simple test

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -28,7 +28,7 @@ DBMS_PASSWORD = getenv('DBMS_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- marketplace/profiling/tests.py
+++ marketplace/profiling/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestItem(TestCase):
+
+    def test_create(self):
+        response = self.client.get('/p/create/')
+        self.assertEqual(response.status_code, 200)
