fdca2c60ddbf1f888d81e3edac89d9c471fa41d9
Vladimir Ermakov <145784290+waldemarX@users.noreply.github.com>
2024-01-20 16:02:02 +0300

    update form for articles

--- marketplace/articles/forms.py
+++ marketplace/articles/forms.py
@@ -7,7 +7,7 @@ from .models import Post
 class ArticleCreationForm(forms.ModelForm):
     image = forms.ImageField(required=False)
     title = forms.CharField()
-    text = forms.Textarea()
+    text = forms.CharField(widget=forms.Textarea)
 
     class Meta:
         model = Post
